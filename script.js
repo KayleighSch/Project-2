0db8b16a26104629ebdecb9df32707fa5e3b2543
Kayleigh Schmassmann <124618333+Schma2k1988@users.noreply.github.com>
2023-07-06 08:56:53 +0100

    added quizDate - questions and answers

---
+++ script.js
@@ -0,0 +1,54 @@
+// Quiz Questions
+
+var quizData = [
+    {
+      country: "Australia",
+      options: ["Sydney", "Melbourne", "Canberra", "Perth"],
+      answer: 2
+    },
+    {
+      country: "Spain",
+      options: ["Madrid", "Barcelona", "Seville", "Valencia"],
+      answer: 0
+    },
+    {
+      country: "Canada",
+      options: ["Toronto", "Vancouver", "Ottawa", "Montreal"],
+      answer: 2
+    },
+    {
+      country: "Brazil",
+      options: ["Rio de Janeiro", "Brasília", "São Paulo", "Salvador"],
+      answer: 1
+    },
+    {
+      country: "Italy",
+      options: ["Rome", "Milan", "Venice", "Florence"],
+      answer: 0
+    },
+    {
+      country: "Japan",
+      options: ["Tokyo", "Kyoto", "Osaka", "Hiroshima"],
+      answer: 0
+    },
+    {
+      country: "South Africa",
+      options: ["Johannesburg", "Cape Town", "Durban", "Pretoria"],
+      answer: 3
+    },
+    {
+      country: "Mexico",
+      options: ["Mexico City", "Guadalajara", "Monterrey", "Cancún"],
+      answer: 0
+    },
+    {
+      country: "India",
+      options: ["New Delhi", "Mumbai", "Kolkata", "Chennai"],
+      answer: 0
+    },
+    {
+      country: "France",
+      options: ["Lyon", "Marseille", "Paris", "Toulouse"],
+      answer: 2
+    },
+];
